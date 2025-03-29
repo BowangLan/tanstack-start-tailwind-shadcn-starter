6f15b89f94cf1ae5515c017c322fb5cbb233dab9
Jeffrey Lan <blan2@uw.edu>
2025-03-29 02:32:47 -0700

    update env

--- app/env.ts
+++ app/env.ts
@@ -1,11 +1,17 @@
 type Env = {
   DATABASE_URL: string;
+  HOST: string;
 };
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not set");
 }
 
+if (!process.env.HOST) {
+  throw new Error("HOST is not set");
+}
+
 export const Env: Env = {
   DATABASE_URL: process.env.DATABASE_URL,
+  HOST: process.env.HOST,
 };
